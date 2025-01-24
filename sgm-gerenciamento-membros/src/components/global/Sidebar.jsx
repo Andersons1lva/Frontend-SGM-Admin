a5f96f3b3601e391d5926b567fda833f92941796
Anderson Silva <andersons1lva@yahoo.com.br>
2025-01-24 14:51:51 -0300

    foi feita a exlusão da pasta node_modules e instalada as dependências novamente, e também criado um css para Sidebar.

--- sgm-gerenciamento-membros/src/components/global/Sidebar.jsx
+++ sgm-gerenciamento-membros/src/components/global/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
+import './pro-sidebar-custom.css';
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../styles/theme";
